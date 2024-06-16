448c46f23a0d28491dfd27a309a11b075914505d
ujjwal9860 <ujjwalbaral63@gmail.com>
2024-06-16 14:38:24 +0545

    changes

--- src/App.js
+++ src/App.js
@@ -1,12 +1,16 @@
 import React from 'react'
 import Header from './ui/Header'
 import Main from './home/Main'
+import Tech from './features/Tech'
+import Info from './features/Info'
 
 const App = () => {
   return (
     <>
       <Header />
       <Main />
+      <Tech />
+      <Info />
     </>
   )
 }
---
+++ src/features/Tech.js
@@ -0,0 +1,24 @@
+import React from 'react'
+import { DiAtom, DiCodepen, DiCss3Full, DiPython, DiReact, DiSwift } from "react-icons/di";
+
+
+const Tech = () => {
+  return (
+    <div className='grid grid-cols-4 justify-items-center lg:g:grid-cols-3 md:grid-cols-2 sm:grid-cols-1'>
+
+
+      <DiReact size={200} className='hover:scale-110 hover:text-pink-700 transition-all ease-in' />
+      <DiPython size={200} className='hover:scale-110 hover:text-pink-700 transition-all ease-in' />
+      <DiCss3Full size={200} />
+      <DiSwift size={200} />
+      <DiAtom size={200} />
+      <DiCodepen size={200} />
+
+
+
+
+    </div>
+  )
+}
+
+export default Tech
